62ccf278128d8c615a70aebadef862a2b9bb2742
salim <salim9@hotmail.nl>
2019-04-18 10:50:08 +0200

    nieuwesite

--- login.php
+++ login.php
@@ -18,6 +18,10 @@
     <title>Inloggen</title>
 </head>
 <style>
+    *{
+      margin: 0px;
+      padding: 0px;
+    }
     body {
         text-align: center;
         background-image: url("./img/FIFA-19-Logo21-1-768x402.jpg");
@@ -25,10 +29,71 @@
         background-size: cover;
         margin: 0;
         padding: 0;
+      font-size: 28px;
+  font-family: 'klarna',sans-serif;
     }
+
+
+
+h1 {
+
+     background-color: #006400;
+     color: #FFFFFF;
+     margin: 2;
+
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
 </style>
+<header>
+    <div class="header">
+  <center><h1>Fifa</h1><center>
+</div>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#team-aanmaken">team-aanmaken</a></li>
+  <li><a href="#overview">overview</a></li>
+  <li><a href="#download">download</a></li>
+  <li><a href="#toernooi">toernooi</a></li>
+  <li><a href="#team-aanpassen">team aanpassen</a></li>
+  <li><a href="#login">login</a></li>
+  <li><a href="#registreren">registreren</a></li>
+</ul>
+<h3>
+
+
+</header>
 <body>
-<h1>Inloggen</h1>
+<h2>Inloggen</h2>
 
 <form action="accountController.php" method="post">
     <input type="hidden" name="type" value="login">
@@ -43,7 +108,20 @@
     </div>
 
     <input type="submit" value="Login">
-    <p>Heb je nog geen account?<a href="register.php">Klik hier</a> om je te registreren</p>
+   <h10> <p>Heb je nog geen account?<a href="register.php">Klik hier</a> om je te registreren</p></h10>
+
+<br>
+<br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+        fifa© 
+        <br>
+       
+
+
+
+</footer>
+
 </form>
 </body>
 </html>
