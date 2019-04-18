b18a8e3111552876329b201b607c6bebec6d6f9e
Unknown <charlotte.oers@kpnmail.nl>
2019-04-18 10:12:46 +0200

    Html en CSS

--- login.php
+++ login.php
@@ -8,6 +8,27 @@
 
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inloggen</title>
+</head>
+<style>
+    body {
+        text-align: center;
+        background-image: url("./img/FIFA-19-Logo21-1-768x402.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin: 0;
+        padding: 0;
+    }
+</style>
+<body>
+<h1>Inloggen</h1>
 
 <form action="accountController.php" method="post">
     <input type="hidden" name="type" value="login">
@@ -22,4 +43,8 @@
     </div>
 
     <input type="submit" value="Login">
+    <p>Heb je nog geen account?<a href="register.php">Klik hier</a> om je te registreren</p>
 </form>
+</body>
+</html>
+
