af87008c11636c79f830f5cd0331e59ea65a1b88
Unknown <charlotte.oers@kpnmail.nl>
2019-04-18 11:12:04 +0200

    Download.php
    HTML en CSS

---
+++ download.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charlotte van Oers
+ * Date: 18-4-2019
+ * Time: 10:44
+ */
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Downloaden</title>
+</head>
+<style>
+    body {
+        text-align: center;
+        margin: auto;
+        background-image: url("./img/FIFA-19-Logo21-1-768x402.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    img {
+        height: 20%;
+        width: 20%;
+    }
+
+    .download {
+        float: center;
+    }
+
+    button {
+        padding: 30px;
+    }
+</style>
+<body>
+    <div class="download">
+        <div class="tekst">
+            <h1>Downloaden</h1>
+        </div>
+        <div class="img">
+            <img src="img/visualstudio.png" alt="">
+        </div>
+        <div class="button">
+            <button>Klik hier om te downloaden</button>
+        </div>
+
+    </div>
+
+</body>
+</html>
