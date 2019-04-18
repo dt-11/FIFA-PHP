e4beb81e9aeecc5b88eac60955a893d880514ad1
Unknown <charlotte.oers@kpnmail.nl>
2019-04-18 11:24:20 +0200

    Toernooi.php
    HTML CSS

---
+++ toernooi.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charlotte van Oers
+ * Date: 18-4-2019
+ * Time: 11:12
+ */
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Toernooi</title>
+</head>
+
+<style>
+
+    body {
+        background-image: url("./img/FIFA-19-Logo21-1-768x402.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        text-align: center;
+    }
+
+    .toernooi p{
+        border: solid 1px;
+        padding: 100px;
+    }
+</style>
+<body>
+<h1>Toernooi</h1>
+
+<div class="toernooi">
+    <p></p>
+</div>
+</body>
+</html>
