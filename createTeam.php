ff9af39fb92a92722889d207a3a6eb131df7cd98
salim <salim9@hotmail.nl>
2019-04-18 11:15:26 +0200

    create team

--- createTeam.php
+++ createTeam.php
@@ -20,16 +20,79 @@
 </head>
 
 <style>
+    *{
+        margin: 0px;
+        padding: 0px;
+    }
     body {
         text-align: center;
         background-image: url("./img/FIFA-19-Logo21-1-768x402.jpg");
         background-repeat: no-repeat;
         background-size: cover;
-    }
+         font-size: 28px;
+  font-family: 'klarna',sans-serif;
+}
+ 
+
+h1 {
+
+     background-color: #4CAF50;
+     color: #FFFFFF;
+     margin: 2;
+
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+
 </style>
 
 <body>
-<h1>Team aanmaken</h1>
+
+<div class="header">
+  <center><h1>Fifa</h1><center>
+</div>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#team-aanmaken">team-aanmaken</a></li>
+  <li><a href="#overview">overview</a></li>
+  <li><a href="#download">download</a></li>
+  <li><a href="#toernooi">toernooi</a></li>
+  <li><a href="#team-aanpassen">team aanpassen</a></li>
+  <li><a href="#login">login</a></li>
+  <li><a href="#registreren">registreren</a></li>
+</ul>
+<h3>
+
+<h2>Team aanmaken</h2>
 
 <form action="accountController.php" method="post">
     <input type="hidden" name="type" value="createTeam">
