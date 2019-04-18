1e1a16b4e6e9407cb70730bfb1486eaab662c356
salim <salim9@hotmail.nl>
2019-04-18 12:24:19 +0200

    login
    l

--- login.php
+++ login.php
@@ -81,7 +81,7 @@ li a:hover {
 <ul>
   <li><a href="index.php">Home</a></li>
   <li><a href="createTeam.php">team-aanmaken</a></li>
-  <li><a href="teamOverview">overview</a></li>
+  <li><a href="teamOverview.php">overview</a></li>
   <li><a href="download.php">download</a></li>
   <li><a href="toernooi.php">toernooi</a></li>
   <li><a href="teamEdit.php">team aanpassen</a></li>
