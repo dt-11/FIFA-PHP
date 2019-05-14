13456ee2514c2f7c92a47bd7322dfd3fea0ee4b2
Unknown <charlotte.oers@kpnmail.nl>
2019-05-14 09:07:49 +0200

    text op index.php en indexLogged.php

--- indexLogged.php
+++ indexLogged.php
@@ -11,6 +11,8 @@ if ( !isset( $_SESSION['sID'] ) ) {
 ?>
 <div class="login_container">
     <h2>Home</h2>
+    <h3>Toernooien</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda atque commodi expedita, facere iste magni molestias officiis pariatur quae. Aut dolorem dolores.</p>
 </div>
 
 </main>
