4a9ece009a49710a4d6a782dd43b698307107721
Youssri <youssdgrebb@gmail.com>
2019-05-23 09:22:15 +0200

    createTournament kan geen dupe maken

--- beta/createTournament.php
+++ beta/createTournament.php
@@ -10,46 +10,46 @@ require 'config.php';
 
 session_start();
 
-$sqlCheckTable = "SELECT * FROM `match-schedule`";
+$sqlCheckTable = "SELECT * FROM `matches`";
 $queryCheckTable = $db->query($sqlCheckTable);
 $checkTable = $queryCheckTable->fetchAll(PDO::FETCH_ASSOC);
 
-//if ($checkTable[0]['poulmade'] == 1){
-//    echo"poul aangemaakt";
-//}
-//else{
-//    echo"Poul nog niet aangemaakt";
-//}
+if ($checkTable[0]['matchCreated'] == 1){
+    header("location: admin.php");
+    exit;
+}
+else{
+    $sql = "SELECT * FROM `teams`";
 
-$sql = "SELECT * FROM teams";
+    $query = $db->query($sql);
 
-$query = $db->query($sql);
+    $teams = $query->fetchAll(PDO::FETCH_ASSOC);
 
-$teams = $query->fetchAll(PDO::FETCH_ASSOC);
-
-$amountOfTeams = count($teams);
+    $amountOfTeams = count($teams);
 
 //$amountOfMatches = ($amountOfTeams * ($amountOfTeams - 1)) / 2;
 
 
-foreach ($teams as $team){
-    $teams = array_slice($teams, 1, $amountOfTeams);
+    foreach ($teams as $team){
+        $teams = array_slice($teams, 1, $amountOfTeams);
 
-    foreach ($teams as $oppositeTeam){
-        $teamA = $team['teamName'];
-        $teamB = $oppositeTeam['teamName'];
+        foreach ($teams as $oppositeTeam){
+            $teamA = $team['teamName'];
+            $teamB = $oppositeTeam['teamName'];
 
-        $sqlCreateMatch = "INSERT INTO matches (teamAId, teamBId)
-                            VALUES (:teamAId, :teamBId)";
-        $prepareCreateMatch = $db->prepare($sqlCreateMatch);
-        $prepareCreateMatch->execute([
-            ":teamAId" => $team['teamId'],
-            ":teamBId" => $oppositeTeam['teamId']
-        ]);
+            $sqlCreateMatch = "INSERT INTO matches (teamAId, teamBId, matchCreated)
+                            VALUES (:teamAId, :teamBId, 1)";
+            $prepareCreateMatch = $db->prepare($sqlCreateMatch);
+            $prepareCreateMatch->execute([
+                ":teamAId" => $team['teamId'],
+                ":teamBId" => $oppositeTeam['teamId']
+            ]);
+        }
     }
+    header("location: tournament.php");
+    exit;
 }
-header("location: tournament.php");
-exit;
+
 
 
 
