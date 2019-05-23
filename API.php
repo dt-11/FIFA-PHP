810c587c3bb341c871375e5522cd258b6ab05369
NotUneay <neallhaek@hotmail.com>
2019-05-23 08:40:25 +0200

    API Hardcoded

--- API.php
+++ API.php
@@ -6,27 +6,15 @@
  * Time: 11:20
  */
 require "config.php";
-header('Content-Type: application/json');
-$testsql = "SELECT * FROM teams";
-$querytest = $db->query($testsql);
-$testing = $querytest->fetchAll(PDO::FETCH_ASSOC);
+
+$key = 'frikandelebakker';
+
+if($_GET['key']) {
+    header('Content-Type: application/json');
+    $testsql = "SELECT * FROM teams";
+    $querytest = $db->query($testsql);
+    $testing = $querytest->fetchAll(PDO::FETCH_ASSOC);
 
 
-echo json_encode($testing);
-
-$sql = "SELECT * FROM users";
-
-$r = mysqli_query($con,$sql);
-$result = array();
-
-while($row = mysqli_fetch_array($r)) {
-    array_push($result, array (
-        'name'=>$row['name'],
-        'email'=>$row['email']
-    ));
-}
-
-if ( !isset( $_SESSION['sID'] ) ) {
-    header('location: download.php');
-    exit;
-}
+    echo json_encode($testing);
+} exit;
--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require "header.php"
 
     <div class="login_container">
         <h2>Home</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi consequuntur, dolorum facere labore magni odio tenetur! Animi eum fuga, fugit laboriosam modi odio.</p>
     </div>
 </main>
 
